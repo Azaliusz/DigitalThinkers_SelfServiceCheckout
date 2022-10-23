fa5b816c9bb7dc136cf65fd306c340950f88422d
Győri Ferenc <gyori.ferenc@unixauto.hu>
2022-10-23 14:16:00 +0200

    DefaultCurrency added to appsettings

--- SelfServiceCheckout/SelfServiceCheckout/Configurations/MoneyOptions.cs
+++ SelfServiceCheckout/SelfServiceCheckout/Configurations/MoneyOptions.cs
@@ -4,6 +4,8 @@ namespace SelfServiceCheckout.Configurations
 {
     public class MoneyOptions
     {
+        public Currencies DefaultCurrency { get; set; }
+
         public Dictionary<Currencies, int[]>? AcceptableDenominations { get; set; }
     }
 }
