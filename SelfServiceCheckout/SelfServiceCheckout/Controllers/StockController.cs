dd398008c34b88387a6b30459d357cbbb109c264
Győri Ferenc <gyori.ferenc@unixauto.hu>
2022-10-23 16:48:43 +0200

    StockController Post method implemented

---
+++ SelfServiceCheckout/SelfServiceCheckout/Controllers/StockController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelfServiceCheckout.Exceptions;
+using SelfServiceCheckout.Services.Abstractions;
+
+namespace SelfServiceCheckout.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+
+        public StockController(IStockService stockService)
+        {
+            _stockService = stockService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] Dictionary<int, int> loadedMoneyDenominations)
+        {
+            try
+            {
+                return Ok(await _stockService.LoadMoneyDenominations(loadedMoneyDenominations));
+            }
+            catch (SelfServiceCheckoutBaseException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
